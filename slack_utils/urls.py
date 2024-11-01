dc2a9d66b29e94219c7a93ff3b581f87f29740ad
Ouadia Lasfar <ouadia.lasfar@gmail.com>
2024-11-01 14:33:28 +0100

    Upgrade to dj 4

--- slack_utils/urls.py
+++ slack_utils/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path as url
 
 from slack_utils import views
 
--- tests/urls.py
+++ tests/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import re_path as url
 
 urlpatterns = [
     url('slack/', include('slack_utils.urls')),
